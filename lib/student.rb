f8056d26a6c58c34318487871fc25528e6c75fae
Jesse Wenger <wengerj@gmail.com>
2021-03-18 12:16:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,17 +22,6 @@ class Student
       self.new(student)
     end
   end
-  
-=begin
-  {:twitter=>"someone@twitter.com",
- :linkedin=>"someone@linkedin.com",
- :github=>"someone@github.com",
- :blog=>"someone@blog.com",
- :profile_quote=>
-  "\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
- :bio=>
-  "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school."}
-=end
 
   def add_student_attributes(attributes_hash)
       self.twitter = attributes_hash[:twitter]
@@ -41,7 +30,6 @@ class Student
       self.blog = attributes_hash[:blog]
       self.profile_quote = attributes_hash[:profile_quote]
       self.bio = attributes_hash[:bio]
-      #binding.pry
   end
 
   def self.all
